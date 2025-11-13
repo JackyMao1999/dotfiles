fde491d218919ddcaa1655a6989cd8c1bfa880ec
maock <maock@yczx.tech>
2025-11-13 11:17:52 +0800

    添加readme,添加一键安装脚本

---
+++ README.md
@@ -0,0 +1,7 @@
+# JackyMao's ubuntu 参数配置
+
+## .config
+- rofi 快捷启动器
+- sway 
+- waybar
+- update.sh 可以更新指定的config数据
---
+++ install.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+# 这是一个一键配置脚本
+
+
+function install_sway() {
+    # 安装sway以及waybar
+
+}
+
+
+
+function main() {
+    install_sway
+
+}
+
+main "$@"
